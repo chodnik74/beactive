575b54c813438aca25df206854cf488f04b14605
Ondrej Ochodnicky <chodnik74@gmail.com>
2017-09-04 19:18:55 +0200

    change alert

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 	$('#carousel').bind('slid.bs.carousel', function (e) {
     var index = $(e.target).find(".active").index();
     if(index === 2)
-        alert('hello world');
+        alert('hello boys');
 	});
 });
 
